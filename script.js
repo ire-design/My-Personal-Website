42508ae0ab7a4b39ba8fa893f6916c9b465f008a
ire-design <irenemwikalii04@gmail.com>
2024-06-22 19:02:35 +0300

    new html css js content

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-/* const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2); */
+/* /*navbar
+var navbar = document.querySelector(".navbar");
+window.onscroll = () =>{
+    this.scrollY > 20? navbar.classList.add(".sticky") : navbar.classList.remove("sticky");
+} */
+
+    // Sticky Navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
